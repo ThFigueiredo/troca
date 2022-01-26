11bcc96f7dd65b84abafa59cc412b8af4cc647e9
ThFigueiredo <thfigueiredo.developer@gmail.com>
2022-01-26 17:13:55 -0300

    Primeiro Commit

---
+++ src/test/java/com/figueiredo/troca/TrocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.figueiredo.troca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
