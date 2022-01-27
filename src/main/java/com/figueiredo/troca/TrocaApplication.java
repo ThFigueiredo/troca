f1ca53b2623076f0e33d8fdc8b21c47570a031bb
ThFigueiredo <thfigueiredo.developer@gmail.com>
2022-01-26 21:59:23 -0300

    Implementação do controller, configuraçao do swagger, implementação do request e response

--- src/main/java/com/figueiredo/troca/TrocaApplication.java
+++ src/main/java/com/figueiredo/troca/TrocaApplication.java
@@ -3,7 +3,18 @@ package com.figueiredo.troca;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
+
+
+
+
 
 @SpringBootApplication
 public class TrocaApplication {
